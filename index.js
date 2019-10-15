d6900eb712618ae4f9968e4571a4154a85b6bebf
Tiago Mendes <tetigo@gmail.com>
2019-10-15 08:29:12 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+colors = require('./colors')
+
+// Front Color Red and Reset
+console.log(colors.fg.Red, 'Text in Red', colors.fg.White)
+console.log('ok')
+// Back Color Red and Reset
+console.log(colors.bg.Red, 'Text in Red', colors.bg.Black)
+console.log('ok')
